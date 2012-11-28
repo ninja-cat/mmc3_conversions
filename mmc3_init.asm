a2b543ff852d671553670ad3aa441ddca1e7f581
Alexander Gordeev <agordeev@mirantis.com>
2012-11-28 16:44:06 +0400

    Add initial mmc3 init

---
+++ mmc3_init.asm
@@ -0,0 +1,14 @@
+;;;
+; perform mmc3 init
+STA $E000 ; IRQ disable
+LDA #$00 ;
+STA $A000 ; Mirroring: vertical
+LDA #$06 ; 
+STA $8000 ; Select 8 KB PRG ROM bank at $8000-$9FFF 
+LDA #$00 ;
+STA $8001; 
+LDA #$07 ;
+STA $8000 ; Select 8 KB PRG ROM bank at $A000-$BFFF
+LDA #$01 ;
+STA $8001 ;
+
